fbc8fb9ddc77938c2362757cab76d986174771c7
Yasuto Tanaka <yasutonaka@gmail.com>
2020-05-15 12:25:15 -0300

    Refactor tests

--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -1,13 +1,14 @@
+# frozen_string_literal: false
+
 require File.join(File.dirname(__FILE__), 'gilded_rose')
 
-describe GildedRose do
-
-  describe "#update_quality" do
-    it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "fixme"
-    end
+describe '#update_quality' do
+  before do
+    @common_items = [Item.new('foo', 0, 0)]
   end
 
+  it 'does not change the name' do
+    GildedRose.new(@common_items).update_quality
+    expect(@common_items.first.name).to eq 'foo'
+  end
 end
